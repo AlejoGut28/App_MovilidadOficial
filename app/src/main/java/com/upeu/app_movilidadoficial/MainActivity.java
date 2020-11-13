541bc0f6f6e7f4ba23bc84801301e1cdecc70644
AlejoGut28 <alejandrogutierrez@upeu.edu.pe>
2020-11-13 00:32:39 -0500

    oficial

---
+++ app/src/main/java/com/upeu/app_movilidadoficial/MainActivity.java
@@ -0,0 +1,32 @@
+package com.upeu.app_movilidadoficial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.sigin);
+
+        redirigir();
+    }
+
+
+    public void redirigir() {
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Nav_Drawer.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+}
